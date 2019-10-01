fa072db86ef68d1746fad108c3b6aab86d40bcc5
GproSoftware828 <JetSendProductions@Gmail.com>
2019-10-01 10:44:04 +1000

    Basic event

---
+++ ReactApp/ClientApp/src/components/EventButton.js
@@ -0,0 +1,32 @@
+﻿import React, { Component } from 'react';
+import '../index.css';
+
+export class EventButton extends Component {
+    constructor(props) {
+        super(props)
+        this.handleEvent = this.handleEvent.bind(this)
+    }
+
+    handleEvent(event) {
+        switch (event.type) {
+            case 'click':
+                console.log('clicked')
+                break
+
+            case 'dblclick':
+                console.log('double clicked')
+                break
+
+            default: 
+                console.log('unhandled', event.type)
+        }
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleEvent} onDoubleClick={this.handleEvent}>Submit Event</button>
+            )
+    }
+}
+
+export default EventButton;
