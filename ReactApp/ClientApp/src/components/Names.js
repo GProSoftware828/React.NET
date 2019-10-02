d71b382f87fc4e6be17e094f33fa0ade36dad99f
GproSoftware828 <JetSendProductions@Gmail.com>
2019-10-02 10:38:08 +1000

    Added third party api data to view page- random person generator

--- ReactApp/ClientApp/src/components/Names.js
+++ ReactApp/ClientApp/src/components/Names.js
@@ -5,6 +5,7 @@ export class Names extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            loading: true,
             data: [
                 { Name1: 'Name1' },
                 { Name2: 'Name2' },
@@ -12,20 +13,36 @@ export class Names extends React.Component {
                 { Name4: 'Name4' },
                 { Name5: 'Name5' },
                 { Name6: 'Name6' }
-            ]
+            ],
+            person: null,
         }
+    }
+
+    async componentDidMount() {
+        const url = "https://api.randomuser.me/";
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({ person: data.results[0], loading: false });
     }
 
     render() {
 
-        const namesList = this.state.data.map(name => {
+        /*const namesList = this.state.data.map(name => {
             console.log(name);
-        })
+        })*/
+
+        if (!this.state.person) {
+            return <div>Didn't find a person</div>;
+        }
 
         return (
-            <ul>
-                <li>Placeholder</li>
-            </ul>
+            <div>
+                {this.state.loading || !this.state.person ? <div>loading...</div> :
+                <div>
+                        <div>{this.state.person.name.first} {this.state.person.name.last}</div>
+                        <div><img src={this.state.person.picture.large} /></div>
+                </div>}
+            </div>
         );
     }
 }
